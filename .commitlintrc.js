e216f88de440107aeef357a97aa029bc64c03750
LaamGinghong <351390560@qq.com>
2020-09-04 16:29:23 +0800

    update: README.md

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['build', 'update', 'fix', 'revert']],
+    'subject-case': [0],
   },
 }
